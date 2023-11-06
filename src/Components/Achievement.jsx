35306ab9af773cd383c72f0960329c9b0ab47c12
Marie-Charlotte LEVITRE <destroyadenyum@gmail.com>
2023-11-06 18:41:52 +0100

    Edit : Achievement (project) component

--- src/Components/Achievement.jsx
+++ src/Components/Achievement.jsx
@@ -1,22 +1,27 @@
+import { useState } from "react";
 
-function Achievement() {
+function Achievement({ title, skills, description, isOpen, onAchievementClick }) {
+    // const [isOpen, setIsOpen] = useState(false);
+
+    function toggleCard() {
+        onAchievementClick();
+    }
+
     return (
-        <article className='card flex-row open'>
+        <article className={`card ${isOpen ? 'open' : ''}`} onClick={toggleCard}>
             {/* <img src='http://i.harperapps.com/covers/9780008108298/y450-293.jpg' class='book'> */}
-            <div className='flex-column info'>
-                <div className='title'>The Fellowship of the Ring</div>
-                <div className='skills'>J.R.R. Tolkien</div>
-                <div className='hidden bottom summary'>
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod ratione impedit temporibus maiores autem aperiam assumenda exercitationem, quisquam nobis esse.
-                </div>
+            <div className='info'>
+                <div className='title'>{title}</div>
+                <div className='description'>{description}</div>
+                <div className='skills'>{skills}</div>
+                <div className='hidden bottom summary'></div>
             </div>
-            <div className='flex-column group'>
+            <div className='group'>
                 <div className='members'>
-                    <span className='current'>14</span> /
-                    <span className='max'>30</span>
+                    <span className='current'>Accompli <i class="fa-solid fa-medal"></i></span>
                 </div>
                 <div className='hidden bottom'>
-                    <button className='simple'>Join</button>
+                    <button className='simple'>Lien GitHub <i class="fa-brands fa-github"></i></button>
                 </div>
             </div>
         </article>
--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+
+import Achievement from "../Components/Achievement";
+
+import dataProject from "../data/dataProject.json";
 
 function Projects() {
-  return (
-    <div className="wrapper">
-      <div class='center list flex-column'> 
+  const [openAchievementIndex, setOpenAchievementIndex] = useState(-1);
+
+  const handleAchievementClick = (index) => {
+    if (index === openAchievementIndex) {
+      // Si le même élément est cliqué, fermez-le en réinitialisant l'index
+      setOpenAchievementIndex(-1);
+    } else {
+      // Sinon, ouvrez le nouvel élément en mettant à jour l'index
+      setOpenAchievementIndex(index);
+    }
+  };
  
+  return (
+    <div className="wrapper projects">
+      <div class='center list flex-column'>
+        {dataProject.map((data, id) => (
+              <Achievement 
+                key={id}
+                title={data.title}
+                description={data.description}
+                isOpen={id === openAchievementIndex}
+                onAchievementClick={() => handleAchievementClick(id)}
+              />
+        ))}
       </div>
     </div>
   )
